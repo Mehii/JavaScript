606ada98277116cbe141c26ba2a6e5724b1c1633
Henrik Horváth <kishasek99@gmail.com>
2022-03-03 13:46:22 +0100

    Value from form

--- Passenger_counter/js/main.js
+++ Passenger_counter/js/main.js
@@ -1,7 +1,14 @@
 let incrementByClick=0;
+let incrementFromInput=0;
+
 function increment()
 {
-  incrementByClick=incrementByClick+10;
-  document.getElementById("count-el").innerHTML=incrementByClick;
+
+  /*incrementByClick=incrementByClick+10;
+  document.getElementById("count-el").innerHTML=incrementByClick;*/
+
+  incrementFromInput=document.getElementById("incrementFromInput").value;
+  document.getElementById("count-el").innerHTML=incrementFromInput;
+
 }
 
